bcb3456b54dd550b2f6572cbb12fdc999bd4fd60
vinicius <vinicius@DESKTOP-BKMT8RP>
2021-09-09 12:44:10 -0300

    Second Commit

--- src/controller/OperacoesControle.java
+++ src/controller/OperacoesControle.java
@@ -13,4 +13,34 @@ public class OperacoesControle {
 		tempoTotal = tempoTotal / Math.pow(10, 9);
 		System.out.println("Tempo para percorrer "+posicoes+" posições: "+tempoTotal+"s.");
 	}
+	
+	public void concatenaBuffer() {
+		StringBuffer buffer = new StringBuffer();
+		double tempoInicial = System.nanoTime();
+		for (int i = 0 ; i < 100000 ; i++) {
+			buffer.append("x");
+		}
+		double tempoFinal = System.nanoTime();
+		double tempoTotal = tempoFinal - tempoInicial;
+		
+		//Converte de millisegundos para segundos.
+		tempoTotal = tempoTotal / Math.pow(10, 9);
+		
+		System.out.println("Buffer ==> "+tempoTotal+"s.");
+		
+	}
+	
+	public void divideFrase(String frase) {
+		String[] vetorFrase = frase.split(" ");
+		int cont = 0;
+		for (String palavra : vetorFrase) {
+			System.out.print(cont+" ");
+			System.out.println(palavra);
+			cont++;
+		}
+	}
+	
+	
+	
+	
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,5 +1,7 @@
 package view;
 
+import javax.swing.JOptionPane;
+
 import controller.OperacoesControle;
 
 public class Principal {
@@ -9,6 +11,11 @@ public class Principal {
 		opCont.concatenaString(1000);
 		opCont.concatenaString(10000);
 		opCont.concatenaString(100000);
+		opCont.concatenaBuffer();
+		
+		//String frase = "Sejam bem vindos à disciplina de Sistemas Operacionais I do 3o Semestre de ADS";
+		String frase = JOptionPane.showInputDialog(null, "Digite uma frase: ");
+		opCont.divideFrase(frase);
 	}
 
 }
